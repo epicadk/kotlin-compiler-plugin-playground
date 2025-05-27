262876e4b379fcc57ba51a7760379d1a529c8a4f
Aditya Kurkure <adityakurkure@gmail.com>
2025-05-27 21:37:25 +0530

    Initial project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("org.jetbrains.kotlin.plugin.compose").version(extra["kotlin.version"] as String)
+    }
+}
+
+rootProject.name = "kotlin-compiler-plugin-playground"
+
+include(":app")
+include(":compiler-plugin")
+include(":compiler-plugin-annotations")
